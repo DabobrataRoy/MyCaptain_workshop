6f9aba10c90d985f889b324874544b9a09bba637
DabobrataRoy <67839102+DabobrataRoy@users.noreply.github.com>
2020-07-20 12:44:23 +0530

    Add files via upload

---
+++ anything.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int age;
+	cout<<"Enter age of person:";
+	cin>>age;
+	 
+	if(age>=18)
+	{
+		cout<<"\nYour are eligible";
+	}
+	else
+	    cout<<"\nYour are not elegible";
+	return 0;
+}
